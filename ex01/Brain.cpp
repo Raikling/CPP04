bb492dc37a4986472e54773ca434c0b03dc6876e
Raikl1ngs <122742770+Raikling@users.noreply.github.com>
2025-12-23 22:52:50 +0100

    brain's done

--- ex01/Brain.cpp
+++ ex01/Brain.cpp
@@ -14,4 +14,27 @@ Brain::Brain(std::string &idea) //why the & here?
     std::cout << "Brain's Parametric constructor called." << std::endl;
 }
 
+Brain::Brain(Brain const& other)
+{
+    for (int i = 0; i < 100; i++)
+        this->_ideas[i] = other._ideas[i];
+    std::cout << "Brain's Copy constructor called." << std::endl;
+}
+
+Brain& Brain::operator=(Brain const& other)
+{
+    if(this != &other)
+    {
+        std::cout << "Brain's Copy assignment operator called." << std::endl;
+        for (int i = 0; i < 100; i++)
+            this->_ideas[i] = other._ideas[i];
+    }
+    return *this;
+}
+
+
+Brain::~Brain()
+{
+    std::cout << "Brain's Deconstructor called." << std::endl;
+}
 
